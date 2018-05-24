292a6cb1089303b907f60a10359bb9d553677793
resuta566 <gijhaycosmod123@gmail.com>
2018-05-24 14:43:31 +0800

    New Update

--- views/car/index.php
+++ views/car/index.php
@@ -9,8 +9,11 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <h1><?= $this->title?></h1>
 <p>
+<?php if(Yii::$app->user->isGuest): ?>
+    <span class="pull-right">Please <?= Html::a('login',['/site/login'])?> to create Car.</span>
+<?php else: ?>
         <?= Html::a('Create Car', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+<?php endif; ?> 
 <table class="table table-bordered">
     <tr>
         <th>ID</th>
--- views/customers/index.php
+++ views/customers/index.php
@@ -9,8 +9,11 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <h1><?= $this->title?></h1>
 <p>
-    <?= Html::a('Create Customer', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+<?php if(Yii::$app->user->isGuest): ?>
+    <span class="pull-right">Please <?= Html::a('login',['/site/login'])?> to create Customer.</span>
+<?php else: ?>
+        <?= Html::a('Create Customer', ['create'], ['class' => 'btn btn-success']) ?>
+<?php endif; ?> 
 <table class="table table-bordered">
     <tr>
          <th>ID</th>
